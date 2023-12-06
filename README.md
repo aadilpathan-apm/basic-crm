5f7742d9f7da225018bf09fd21ce95e1f1afbafc
JPM-LS <javed.pathan@lynkersoft.com>
2023-12-07 00:16:39 +0530

    NEW: Customer model done.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # basic-crm
+
+# for create 
+# python manage.py makemigrations
+# python manage.py migrate
+
+# for create super user 
+# python manage.py createsuperuser
+
+# for create new module
+# python manage.py startapp <MODULE-NAME>
--- customers/models.py
+++ customers/models.py
@@ -1,15 +1,17 @@
-from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
+import uuid
 
 class Customer(models.Model):
-    customer_id: models.UUIDField(primary_key=True, auto_created=True, editable=False)
-    company_name: models.CharField(max_length=150)
-    created_date: models.DateField(null=False, default=datetime.today)
-    creator_id: models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    changed_date: models.DateField(null=False, default=datetime.today)
-    changer_id: models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    is_deleted: models.BooleanField(null=False, default=False)
-    deleted_date: models.DateField(null=True, default=datetime.today)
-    deleter_id: models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    why_deleted: models.TextField(max_length=500)
+    customer_id = models.UUIDField(primary_key=True, default=uuid.uuid4, auto_created=True, editable=False)
+    company_name = models.CharField(null=False, max_length=150)
+    email_address = models.EmailField(null=True)
+    mobile_number = models.CharField(null=True, max_length=15)
+    created_date = models.DateTimeField(null=False)
+    creator = models.ForeignKey(User, null=False, related_name='created_by', on_delete=models.CASCADE)
+    changed_date = models.DateTimeField(null=False)
+    changer = models.ForeignKey(User, null=False, related_name='changed_by', on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(null=False, default=False)
+    deleted_date = models.DateTimeField(null=True)
+    deleter = models.ForeignKey(User, null=True, related_name='deleted_by', on_delete=models.CASCADE)
+    why_deleted = models.TextField(null=True, max_length=500)
